b25c1cf522ab7c9f4bcb13afc3b3c268f11736ce
Natalia_M <n.matat@gmail.com>
2022-06-12 00:17:50 +0300

    added current city time

--- src/app.js
+++ src/app.js
@@ -1,10 +1,37 @@
+function formatDate(timestamp) {
+	let date = new Date(timestamp);
+	let hours = date.getHours();
+	if (hours < 10) {
+		hours = `0${hours}`;
+	}
+
+	let minutes = date.getMinutes();
+
+	if (minutes < 10) {
+		minutes = `0${minutes}`;
+	}
+
+	let days = [
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday",
+	];
+
+	let day = days[date.getDay()];
+	return `${day}, ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
-	console.log(response.data);
+	//console.log(response.data);
 	let temperatureElement = document.querySelector("#temperature");
 	temperatureElement.innerHTML = Math.round(response.data.main.temp);
 
 	let cityElement = document.querySelector("#city");
-	cityElement.innerHTML = response.data.name;
+	cityElement.innerHTML = `${response.data.name}, ${response.data.sys.country}`;
 
 	let descriptionElement = document.querySelector("#description");
 	descriptionElement.innerHTML = response.data.weather[0].description;
@@ -14,9 +41,13 @@ function displayTemperature(response) {
 
 	let windElement = document.querySelector("#wind");
 	windElement.innerHTML = Math.round(response.data.wind.speed);
+
+	let dateElement = document.querySelector("#date");
+	dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
+let city = "New York";
 let apiKey = "15b17c39bc6708ab7518942a1ffb9aca";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Riga&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
