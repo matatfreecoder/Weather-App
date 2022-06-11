f31863e847b28126c11f95377181858fb7d2b204
Natalia_M <n.matat@gmail.com>
2022-06-11 23:42:02 +0300

    added api integration weather

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
-//alert("Hello");
+function displayTemperature(response) {
+	console.log(response.data);
+	let temperatureElement = document.querySelector("#temperature");
+	temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+	let cityElement = document.querySelector("#city");
+	cityElement.innerHTML = response.data.name;
+
+	let descriptionElement = document.querySelector("#description");
+	descriptionElement.innerHTML = response.data.weather[0].description;
+
+	let humidityElement = document.querySelector("#humidity");
+	humidityElement.innerHTML = response.data.main.humidity;
+
+	let windElement = document.querySelector("#wind");
+	windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "15b17c39bc6708ab7518942a1ffb9aca";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Riga&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
