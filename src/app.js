0c266e648000bdd5d8c3d026c2a9ae34e39326a8
Natalia_M <n.matat@gmail.com>
2022-06-12 23:22:32 +0300

    added background

--- src/app.js
+++ src/app.js
@@ -65,7 +65,17 @@ function handleSubmit(event) {
 	search(cityInputElement.value);
 }
 
+function displayFahrenheitTemperature(event) {
+	event.preventDefault();
+	let fahrenheitTemperature = (14 * 9) / 5 + 32;
+	let temperatureElement = document.querySelector("#temperature");
+	temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
 search("kyiv");
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
