e5c2ba92abb089d99b70dfc0ea44a76f9373b784
Natalia_M <n.matat@gmail.com>
2022-06-15 23:27:36 +0300

    connected api to forecast

--- src/app.js
+++ src/app.js
@@ -25,7 +25,8 @@ function formatDate(timestamp) {
 	return `${day}, ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+	console.log(response.data.daily);
 	let forecastElement = document.querySelector("#forecast");
 	let forecastHTML = "";
 	let days = [
@@ -59,6 +60,14 @@ function displayForecast() {
 	forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+	console.log(coordinates);
+	let apiKey = "15b17c39bc6708ab7518942a1ffb9aca";
+	let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+	console.log(apiUrl);
+	axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
 	//console.log(response.data);
 	let temperatureElement = document.querySelector("#temperature");
@@ -85,6 +94,7 @@ function displayTemperature(response) {
 		"src",
 		`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
 	);
+	getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -121,8 +131,6 @@ function displaycelsiusTemperature(event) {
 
 let celsiusTemperature = null;
 search("kyiv");
-
-displayForecast();
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
